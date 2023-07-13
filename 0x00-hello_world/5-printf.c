b539f8c2d8e2f80cf46205dfbe56f8f7dce8c1af
Colightech <saintchris4@gmail.com>
2023-07-13 04:26:44 -0700

    my 5-printf.c task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints line of text using printf
+ * Return: 0 (this is end of the program)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
