5fd7db26997e7c344d0539abf1b2ed84dbfad847
Colightech <saintchris4@gmail.com>
2023-07-13 10:35:32 -0700

    my 101-quote.c task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  *  main -  this program prints line of text
- *  Return: 1 (this cannote success)
+ *  Return: 1 (for success)
  */
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
