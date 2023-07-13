25d1714e6553f2e6dbf4e3a2f549a2bc1c009853
Colightech <saintchris4@gmail.com>
2023-07-13 03:57:46 -0700

    my 4-puts.c task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints a line of text using puts function
+ * Return: 0 (this connte that the program success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
