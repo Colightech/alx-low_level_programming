27a0c888f4aee9fd6f7fd34b8895e7874efc1796
Colightech <saintchris4@gmail.com>
2023-07-14 12:31:01 -0700

    my 7-print_tebahpla.c task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program entring
+ * Return: 0 (cannote success)
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
