a7b4beb257a0feaaef7470a9446b23c372dca7ef
Colightech <saintchris4@gmail.com>
2023-07-14 09:08:43 -0700

    my 5-print_numbers.c task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - my program start here
+ * Return: 0 (complet)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
