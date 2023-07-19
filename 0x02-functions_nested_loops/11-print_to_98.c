f9c8b90e77eee0f172f5e7aa123d3a52b2e9f6bf
Colightech <saintchris4@gmail.com>
2023-07-18 17:02:02 -0700

    my 101-natural.c task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,39 @@
 */
 
 void print_to_98(int n)
+
 {
-	int i;
+
+	int i, j;
 
 	if (n <= 98)
 	{
-		printf("%d", n);
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+		}
+
+		else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+				{
+					printf("%d, ", j);
+				}
+
+				else if (j == 98)
+				{
+					printf("%d\n", j);
+				}
+			}
+		}
 	}
-	for (i = n + 1; i <= 98; i++)
-	{
-		printf(", %d", i);
-	}
-	printf("\n");
 }
