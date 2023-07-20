29328bc91657482d3ad14afc91f334e1a2c3a6a2
Colightech <saintchris4@gmail.com>
2023-07-20 12:15:12 -0700

    my 9-fizz_buzz.c task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the numbers from 1 to 100
+ * Return: 0 successful
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("%s", "FizzBuzz");
+		else if (n % 3 == 0)
+			printf("%s", "Fizz");
+		else if (n % 5 == 0)
+			printf("%s", "Buzz");
+		else
+			printf("%d", n);
+		if (n != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
