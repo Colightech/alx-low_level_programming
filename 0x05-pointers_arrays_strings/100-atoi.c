0d9212b23d6bc6c40c84cc874f3993e88c6d4ff0
Colightech <saintchris4@gmail.com>
2023-07-24 12:38:31 -0700

    my 100-atoi.c task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi -  function that convert a string to an integer
+ * @s: string pointer
+ * Return: 0 un success
+ */
+
+int _atoi(char *s)
+
+{
+	int x = 0;
+	unsigned int n = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[x])
+	{
+		if (s[x] == 45)
+		{
+			min *= -1;
+		}
+		while (s[x] >= 48 && s[x] <= 57)
+		{
+			isi = 1;
+			n = (n * 10) + (s[x] - '0');
+			x++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		x++;
+	}
+	n *= min;
+	return (n);
+}
