c299104164368777d9a9d8befa4687dcaf0a4f20
Colightech <saintchris4@gmail.com>
2023-08-14 07:22:14 -0700

    my 2-main.c task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the name of the file
+ * Return: 0 on success
+ */
+
+int main(void)
+
+
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
