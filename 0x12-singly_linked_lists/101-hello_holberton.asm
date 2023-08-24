1ff166fe330d15a0cc631db0b87e0cb61c26ba85
Colightech <saintchris@gmail.com>
2023-08-24 15:27:24 -0700

    my 101-hello_holberton.asm task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,16 @@
-global   main
-	  extern    printf
+section .data
+    format db "Hello, Holberton",10,0  ; The format string with newline
+
+section .text
+    global main
+    extern printf
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov   eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+    sub rsp, 8             ; Align the stack to 16 bytes boundary
+    mov rdi, format        ; Load the address of the format string
+    call printf           ; Call the printf function
+    add rsp, 8             ; Restore the stack pointer
+    mov eax, 0x60          ; syscall: exit
+    xor edi, edi           ; status: 0
+    syscall
+
