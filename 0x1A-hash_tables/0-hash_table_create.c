e055f121a91ca14234581f5a0fb7febf91978b8c
Colightech <saintchris4@gmail.com>
2023-11-04 05:45:13 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,17 +8,17 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *h_table = 0;
+	hash_table_t *h_table;
 
 	if (size == 0)
 		return (NULL);
 
-	h_table = calloc(1, sizeof(hash_table_t));
+	h_table = malloc(sizeof(hash_table_t));
 	if (h_table == NULL)
 		return (NULL);
 
 	h_table->size = size;
-	h_table->array = calloc(size, sizeof(hash_node_t));
+	h_table->array = malloc(sizeof(hash_table_t) * size);
 	if (h_table->array == NULL)
 	{
 		free(h_table);
